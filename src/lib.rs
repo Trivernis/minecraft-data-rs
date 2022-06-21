c63cd1f11a4b1cc830b46e0a9d0d28f96400d17b
Wyatt Herkamp <wherkamp@gmail.com>
2022-06-21 07:56:21 -0400

    Allows you to remove the minecraft-data at compile time

--- src/lib.rs
+++ src/lib.rs
@@ -4,12 +4,15 @@
 extern crate serde_derive;
 
 /// Provides data access methods
+#[cfg(feature="include_data")]
 pub mod api;
+#[cfg(feature="include_data")]
 pub(crate) mod data;
 /// Contains the type definitions for the data
 pub mod models;
 pub(crate) mod utils;
 
+#[cfg(feature="include_data")]
 pub use api::Api;
 pub use utils::error::DataError;
 pub use utils::error::DataResult;
