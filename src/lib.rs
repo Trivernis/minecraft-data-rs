47e95167ef29b0cf0c4034da6daddf675b1ef590
Wyatt Herkamp <wherkamp@gmail.com>
2022-06-21 09:59:16 -0400

    Rename the feature

--- src/lib.rs
+++ src/lib.rs
@@ -4,15 +4,15 @@
 extern crate serde_derive;
 
 /// Provides data access methods
-#[cfg(feature="include_data")]
+#[cfg(feature="include-data")]
 pub mod api;
-#[cfg(feature="include_data")]
+#[cfg(feature="include-data")]
 pub(crate) mod data;
 /// Contains the type definitions for the data
 pub mod models;
 pub(crate) mod utils;
 
-#[cfg(feature="include_data")]
+#[cfg(feature="include-data")]
 pub use api::Api;
 pub use utils::error::DataError;
 pub use utils::error::DataResult;
